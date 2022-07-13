ae87f330f03640365b0bcefbdc835302832c59e0
Shaance <christophe.ha1@gmail.com>
2022-07-13 18:45:47 +0200

    Added required query param nbFiles to requests call

--- lib/canaries/requests/index.ts
+++ lib/canaries/requests/index.ts
@@ -3,7 +3,7 @@ import { get } from 'request'
 const url = process.env.REQUESTS_API_URL as string;
 
 async function getRequest() {
-  get(url, { json: true }, (err, res) => {
+  get(url + '?nbFiles=5', { json: true }, (err, res) => {
     if (err) {
       console.log(err);
       throw new Error(`Error! status: ${err.status}`);
